e212437d7bf8b93fe4aef615c26a49d7fe95a27e
That Li <thatli3163@gmail.com>
2026-08-30 03:17:46 +0800

    Updated template with new bot name

---
+++ src/main/java/Dobby.java
@@ -0,0 +1,21 @@
+/**
+ * Starts the Dobby chatbot application.
+ */
+public class Dobby {
+    /**
+     * Displays a welcome banner when the chatbot starts.
+     *
+     * @param args command-line arguments (not used)
+     */
+    public static void main(String[] args) {
+        String banner = "       *       .       *       .       *       \n"
+                + "   .      ____        _     _              .   \n"
+                + "     *   |  _ \\  ___ | |__ | |__  _   _     * \n"
+                + "   .     | | | |/ _ \\| '_ \\| '_ \\| | | |   . \n"
+                + "     *   | |_| | (_) | |_) | |_) | |_| |     * \n"
+                + "   .     |____/ \\___/|_.__/|_.__/ \\__, |   . \n"
+                + "                                  |___/        \n"
+                + "       *       .       *       .       *       \n";
+        System.out.println(banner);
+    }
+}
