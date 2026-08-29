ee98f5c6e50092683b1e949dcedaed6029cf1b26
That Li <thatli3163@gmail.com>
2026-08-30 03:28:40 +0800

    Added greeting and exit texts

--- src/main/java/Dobby.java
+++ src/main/java/Dobby.java
@@ -8,6 +8,7 @@ public class Dobby {
      * @param args command-line arguments (not used)
      */
     public static void main(String[] args) {
+        String lines = "____________________________________________________________";
         String banner = "       *       .       *       .       *       \n"
                 + "   .      ____        _     _              .   \n"
                 + "     *   |  _ \\  ___ | |__ | |__  _   _     * \n"
@@ -16,6 +17,15 @@ public class Dobby {
                 + "   .     |____/ \\___/|_.__/|_.__/ \\__, |   . \n"
                 + "                                  |___/        \n"
                 + "       *       .       *       .       *       \n";
-        System.out.println(banner);
+        print(lines);
+        print(banner);
+        print("Dobby says hi!");
+        print("Dobby is ready to take orders.");
+        print(lines);
+        print("Dobby says goodbye to master!");
+    }
+
+    private static void print(String str) {
+        System.out.println(str);
     }
 }
