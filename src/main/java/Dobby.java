379d8e8a52d67c8e052d77c95862fe720c68ae15
That Li <thatli3163@gmail.com>
2026-08-31 11:55:20 +0800

    Remove comments

--- src/main/java/Dobby.java
+++ src/main/java/Dobby.java
@@ -38,15 +38,6 @@ public class Dobby {
         print(line);
     }
 
-    // Class for Tasks:
-    // will be stored in java collection
-    // 2 states: done, not done
-    // stores String Description
-    // subtypes -> ToDos, Events, Deadlines
-    // -> each stores String task, + deadline date, + start & end date
-    // handle error
-    // can be deleted
-
     private static void print(String str) {
         DobbyUtil.print(str);
     }
