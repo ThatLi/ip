d719ef2a33a41e6fff0f06e769d776698eb0d3e1
That Li <thatli3163@gmail.com>
2026-08-31 11:40:41 +0800

    Codex implementation

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -10,6 +10,6 @@ public class Event extends Task {
 
     @Override
     public String toString() {
-        return super.toString() + " (from: " + from + " to" + to + ")";
+        return super.toString() + " (from: " + from + " to: " + to + ")";
     }
 }
