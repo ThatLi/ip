eaba53b3093e6c982d0e6d3504c5b0aae7f2ea52
That Li <thatli3163@gmail.com>
2026-09-05 23:43:25 +0800

    Setup Junit test skeleton

---
+++ src/test/java/dobby/logic/DobbyLogicTest.java
@@ -0,0 +1,12 @@
+package dobby.logic;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DobbyLogicTest {
+
+    @Test
+    public void deleteTask_validIndex_success() {
+        // assertEquals of a test case
+    }
+}
